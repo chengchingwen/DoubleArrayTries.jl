b895745ef621db515594e5e37e386886f951e178
Documenter.jl <documenter@juliadocs.github.io>
2022-10-17 11:34:42 +0000

    build based on c67ef9b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DoubleArrayTries","category":"page"},{"location":"#DoubleArrayTries","page":"Home","title":"DoubleArrayTries","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DoubleArrayTries.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DoubleArrayTries]","category":"page"}]
+}
